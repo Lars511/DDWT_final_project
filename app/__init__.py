57d99ce3fd44aa1c650ec5fcc1ee214e7833269d
lars511 <191249734+Lars511@users.noreply.github.com>
2026-01-09 11:17:09 +0100

    Added a setup for Flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,37 @@
 """
-Initializing the database and setting the secret key
+Setup for Flask
 """
+from flask import Flask, render_template
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from flask_migrate import Migrate
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Getting the secret key
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+# Configure SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(
+    basedir, 'instance/movies.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Getting the secret key
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+# Initializing flask login
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes
+from . import models
