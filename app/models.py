df790cbbd59f435056f6af8a917386c5920f6017
lars511 <191249734+Lars511@users.noreply.github.com>
2026-01-09 11:22:52 +0100

    Added a basic user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,37 @@
 """
 Setting up the classes for users, activities and the api
 """
+from flask import url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+from app import db
+from datetime import datetime, timedelta, timezone 
+from typing import Optional
+import secrets
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+
+class Users(UserMixin, db.Model):
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    token: so.Mapped[Optional[str]] = so.mapped_column(
+        sa.String(32), index=True, unique=True)
+    token_expiration: so.Mapped[Optional[datetime]]
+
+    # Password hashing
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# Loading the user
+@login.user_loader
+def load_user(id):
+    return db.session.get(Users, int(id))
