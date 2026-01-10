d3468225a9570d9167894774e090d38776cd8151
papanoroz <pnpn1915@gmail.com>
2026-01-10 12:15:35 +0100

    fixed the run.py, and added routes

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,33 @@ from flask import render_template
 from app import app
 
 @app.route('/', methods=['GET'])
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/activities")
+def activities():
+    return render_template("activities.html")
+
+
+@app.route("/activities/create")
+def create_activity():
+    return render_template("create_activity.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
