ff62f39b7ea832941823f42b807d73a1d49a27c5
papanoroz <pnpn1915@gmail.com>
2026-01-10 18:39:28 +0100

    added add/edit/delete functionality, also added dropdown menu dor categories, and added categories to the database

--- app/routes.py
+++ app/routes.py
@@ -1,91 +1,157 @@
 """
 Setting up the routes for the html pages
-Needs:
-- Login
-- home
-- registering new user
-- adding activities
-- deleting activities
-- logout
-- joining activities
 """
 
 from flask import render_template, flash, redirect, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from app import db, app
 from app.forms import LoginForm, SignUpForm
-from app.models import Users
+from app.models import Users, Activity, Category
 import sqlalchemy as sa
 from urllib.parse import urlsplit
-from app.models import Activity
+from datetime import date, time
 
+
+# HOME
 @app.route('/', methods=['GET'])
 @app.route('/index')
 @login_required
 def index():
     return render_template('index.html')
 
-@app.route("/login", methods=["POST", "GET"])
+
+# LOGIN
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        user = db.session.scalar(sa.select(Users).where(Users.username ==
-        form.username.data))
+        user = db.session.scalar(
+            sa.select(Users).where(Users.username == form.username.data)
+        )
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
+
         login_user(user, remember=form.remember_me.data)
+
         next_page = request.args.get('next')
-        # Make sure the user is redirected if the link is not valid or authorised
         if not next_page or urlsplit(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
+
     return render_template('login.html', title='Sign In', form=form)
 
-#logout
+
+# LOGOUT
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
+
+# REGISTER
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
+
     form = SignUpForm()
     if form.validate_on_submit():
         user = Users(username=form.username.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Sign up succesful! You can now log in!')
+        flash('Sign up successful! You can now log in.')
         return redirect(url_for('login'))
+
     return render_template('register.html', title='Register', form=form)
 
 
+# READ ACTIVITIES
 @app.route("/activities")
 @login_required
 def activities():
     activities = Activity.query.all()
     return render_template("activities.html", activities=activities)
 
-@app.route("/activities/create")
+
+@app.route("/activities/create", methods=["GET", "POST"])
 @login_required
 def create_activity():
-    return render_template("create_activity.html")
+    categories = Category.query.all()
+
+    if request.method == "POST":
+        activity = Activity(
+            title=request.form["title"],
+            description=request.form["description"],
+            location=request.form["location"],
+            activity_date=date.fromisoformat(request.form["activity_date"]),
+            activity_time=time.fromisoformat(request.form["activity_time"]),
+            max_participants=int(request.form["max_participants"]),
+            category_id=int(request.form["category_id"]),
+            creator_id = current_user.id
+        )
+        db.session.add(activity)
+        db.session.commit()
+        flash("Activity created successfully!")
+        return redirect(url_for("activities"))
+
+    return render_template(
+        "create_activity.html",
+        categories=categories
+    )
+
+# EDIT ACTIVITY (GET + POST)
+@app.route("/activities/<int:id>/edit", methods=["GET", "POST"])
+@login_required
+def edit_activity(id):
+    activity = Activity.query.get_or_404(id)
+    categories = Category.query.all()
+
+    if request.method == "POST":
+        activity.title = request.form["title"]
+        activity.description = request.form["description"]
+        activity.location = request.form["location"]
+        activity.activity_date = date.fromisoformat(request.form["activity_date"])
+        activity.activity_time = time.fromisoformat(request.form["activity_time"])
+        activity.max_participants = int(request.form["max_participants"])
+        activity.category_id = int(request.form["category_id"])
+
+        db.session.commit()
+        flash("Activity updated successfully!")
+        return redirect(url_for("activities"))
+
+    return render_template(
+        "edit_activity.html",
+        activity=activity,
+        categories=categories
+    )
 
 
+
+# DELETE ACTIVITY
+@app.route("/activities/<int:id>/delete", methods=["POST"])
+@login_required
+def delete_activity(id):
+    activity = Activity.query.get_or_404(id)
+    db.session.delete(activity)
+    db.session.commit()
+    flash("Activity deleted.")
+    return redirect(url_for("activities"))
+
+
+# CATEGORIES
 @app.route("/categories")
 @login_required
 def categories():
     return render_template("categories.html")
 
 
+# ERROR
 @app.route("/error")
 def error():
     return render_template("error.html")
-
-if __name__ == '__main__':
-    app.run(debug=True)
