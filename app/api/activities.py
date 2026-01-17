eb7cf21eec18cc50a3a02bdda7853860cec4f8c7
lars511 <191249734+Lars511@users.noreply.github.com>
2026-01-17 15:42:37 +0100

    Added get_category()

--- app/api/activities.py
+++ app/api/activities.py
@@ -15,7 +15,26 @@ def get_activities():
     return Activity.to_collection_dict(sa.select(Activity), page, per_page,
                                    'api.get_activities')
 
-@bp.route('/profile/<name>', methods=['GET'])
-def get_user(name):
-    pass
+""" 
+Categories are numbered in the following way:
+1 = sport and fitness
+2 = study and work 
+3 = casual and social
+4 = outdoor and leisure
+5 = hobbies and entertainment
+6 = university and student life
+7 = other/custom
+"""
+@bp.route('categories/<int:id>')
+def get_category(id):
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    return Activity.to_collection_dict(sa.select(Activity).where(Activity.category_id == id), page, per_page,
+                                   'api.get_category', id=id)
 
+@bp.route('/profile/<name>', methods=['GET'])
+def created_activities(name):
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    return Activity.to_collection_dict(sa.select(Activity).where(Activity.creator_id == name), page, per_page,
+                                   'api.created_activities', name=name)
